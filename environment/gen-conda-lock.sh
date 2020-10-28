8e213f2b95001fecf25c25155b03a4c0b70e46f3
matt-long <mclong@ucar.edu>
2020-10-28 09:04:23 -0600

    clean comment

--- environment/gen-conda-lock.sh
+++ environment/gen-conda-lock.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
+# This script generates a conda lock file, which can be used to create an "exact" replica
+# of our analysis kernal environment
+
 conda_env=$(grep name: environment.yml | awk '{print $2}')
 
 source activate ${conda_env}
 
 # generate the lockfiles
 conda-lock -f environment.yml -p osx-64 -p linux-64
-
-cat << EOF
-# create an environment from the lockfile
-conda-lock install [-p {prefix}|-n {name}] conda-linux-64.lock
-
-# alternatively, use conda command directly
-conda create -n my-locked-env --file conda-linux-64.lock
-EOF
