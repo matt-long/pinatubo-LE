ecde46c9c70d1fbc5aaaf131276ca20efe6d0c72
Michael Levy <mike.levy.work@gmail.com>
2020-12-16 13:57:57 -0700

    Tweaks to RCP script and start of reshaper
    RCP cases use BRCP85C5CNBDRD in case name and the only xml change necessary is
    updating the RUN_REFCASE; everything else is correct by default.
    
    Data reshaper currently only works for monthly history files. For each
    component, it makes time series for each variable broken into two files: 1990 -
    2005 and 2006 - 2025.
    
    Still to do:
    -- shell scripts for annual and daily streams
    -- setting up directory in campaign to organize all the data (will mirror LENS
       data layout)
    -- automating transfer to campaign

---
+++ data_reshaper/monthly.sh
@@ -0,0 +1,131 @@
+#!/bin/bash -l
+#
+#SBATCH -n 64
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=16
+#SBATCH -t 1:00:00
+#SBATCH -p dav
+#SBATCH --account=P93300606
+#SBATCH --mem 100G
+#SBATCH -m block
+#
+module purge
+conda deactivate || echo "conda not loaded"
+#
+# PARSE COMMAND LINE ARGUMENTS
+if [ $# != 5 ]; then
+  echo "ERROR: got $# arguments"
+  echo "usage: $0 CASE ARCHIVE_ROOT START_YEAR END_YEAR COMPONENT"
+  exit 1
+fi
+
+CASE=${1} ; export CASE
+ARCHIVE_ROOT=${2}
+START_YEAR=${3}
+END_YEAR=${4}
+COMPONENT=${5}
+echo "Reshaping ${COMPONENT} output for years ${START_YEAR} through ${END_YEAR} for ${CASE}..."
+#
+cd /glade/p/cesm/postprocessing_dav/cesm-env2/bin
+. activate
+#
+module load intel/17.0.1
+module load ncarenv
+module load ncarcompilers
+module load impi
+module load netcdf/4.6.1
+module load nco/4.7.4
+module load ncl/6.4.0
+#
+case "$COMPONENT" in
+  pop )
+    HIST=pop.h ;;
+  cice )
+    HIST=cice.h ;;
+  cam )
+    HIST=cam.h0 ;;
+  clm )
+    HIST=clm2.h0 ;;
+  rtm )
+    HIST=rtm.h0 ;;
+  * )
+    echo "Unknown component ${COMPONENT}"
+    exit 1 ;;
+esac
+export HIST
+
+PATH=/glade/p/cesm/postprocessing_dav/cesm-env2/bin:/usr/local/bin:${PATH} ; export PATH
+#
+NCKS=`which ncks`  ; export NCKS
+PROCHOST=`hostname`;export PROCHOST
+#
+BASEDIR=/glade/u/home/strandwg/CCP_Processing_Suite
+LOCALDSK=${ARCHIVE_ROOT}/${CASE} ; export LOCALDSK
+PROCBASE=/glade/scratch/$USER/reshaper/${CASE}     ; export PROCBASE
+#
+HTYP=`echo $HIST | cut -d'.' -f1` ; export HTYP
+case "$HTYP" in
+  cam2 | cam )
+    COMP_NAME=atm ;;
+  cism )
+    COMP_NAME=glc ;;
+  clm2 )
+     COMP_NAME=lnd ;;
+  pop  )
+    COMP_NAME=ocn ;;
+  rtm | mosart )
+    COMP_NAME=rof ;;
+  cice | csim )
+    COMP_NAME=ice ;;
+  * )
+    echo "Unable to continue because "$HIST" not known."
+    exit 1 ;;
+esac
+#
+LOCAL_HIST=${LOCALDSK}/${COMP_NAME}/hist ; export LOCAL_HIST
+LOCAL_PROC=${PROCBASE}/${HIST}/proc      ; export LOCAL_PROC
+CACHEDIR=${LOCAL_PROC}/COMPLETED         ; export CACHEDIR
+#
+VERBOSITY=0 ; export VERBOSITY
+PREFIX="${CACHEDIR}/${CASE}.${HIST}." ; export PREFIX
+NCFORMAT=netcdf4c ; export NCFORMAT ; export NCFORMAT
+#
+if [ ! -d $LOCAL_PROC ] ; then
+ mkdir -p $LOCAL_PROC
+fi
+if [ ! -d $CACHEDIR ] ; then
+ mkdir -p $CACHEDIR
+fi
+#
+cd $LOCAL_PROC
+ln -s -f $BASEDIR/run_slice2series_dav Transpose_Data
+#
+rm -f ${CASE}.${HIST}.*nc
+if [ ! -f ${LOCAL_PROC}/.DONE.${CASE}.${HIST}.${START_YEAR}_${END_YEAR} ] ; then
+  HISTF=
+  for YEAR in $(seq ${START_YEAR} ${END_YEAR}) ; do
+    echo "YEAR: ${YEAR}"
+    echo "LINKING FROM ${LOCAL_HIST}"
+    ln -s -f ${LOCAL_HIST}/${CASE}.${HIST}.${YEAR}*nc .
+    HISTF+="${CASE}.${HIST}.${YEAR}*nc "
+  done
+  NHISTF=`/bin/ls ${HISTF} | wc -l`
+  if [ $(expr $NHISTF % 12) ] ; then
+    OUTTIME="${START_YEAR}01-${END_YEAR}12"
+    SUFFIX=".${OUTTIME}.nc" ; export SUFFIX
+    echo -n "TS transpose_data start: " ; date
+    ./Transpose_Data
+    if [ $? -ne 0 ] ; then
+      echo "Transpose_Data failed"
+      exit 1
+    fi
+    echo -n "TS transpose_data end  : " ; date
+    touch ${LOCAL_PROC}/.DONE.${CASE}.${HIST}.${START_YEAR}_${END_YEAR}
+  else
+    echo "File count mismatch on "${CASE}"."${HIST}"."${START_YEAR}_${END_YEAR}": "${NHISTF}" instead of multiple of 12"
+  fi
+fi
+#
+echo -n "TS COMPLETE: " ; date
+#
+exit
---
+++ data_reshaper/run_all.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+import os
+
+
+def _parse_args():
+    """ Parse command line arguments """
+
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Submit scripts to reshape highres BGC output",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # Optional: specify start year
+    parser.add_argument(
+        "-y",
+        "--start-year",
+        action="store",
+        dest="start_year",
+        type=int,
+        default=1990,
+        help="First year of run to convert to time series",
+    )
+
+    # Optional: specify end year
+    parser.add_argument(
+        "-e",
+        "--end-year",
+        action="store",
+        dest="end_year",
+        type=int,
+        default=2025,
+        help="Last year of run to convert to time series",
+    )
+
+    # Optional: specify ensemble member
+    parser.add_argument(
+        "-m",
+        "--ensemble-member",
+        action="store",
+        dest="ensemble_member",
+        type=int,
+        default=1,
+        help="Suffix of case to convert to time series",
+    )
+
+    # Optional: specify component(s) to run
+    parser.add_argument(
+        "-c",
+        "--components",
+        action="store",
+        dest="components",
+        type=str,
+        nargs="+",
+        default=[
+            "pop",
+            "cice",
+            "cam",
+            "clm",
+            "rtm",
+        ],
+        help="Scripts to submit to slurm",
+    )
+
+    # Optional: location of DOUT_S_ROOT
+    archive_default = os.path.join(
+        os.sep, "glade", "scratch", os.environ["USER"], "archive"
+    )
+    parser.add_argument(
+        "-a",
+        "--archive-root",
+        action="store",
+        dest="archive_root",
+        type=str,
+        default=archive_default,
+        help="base of DOUT_S_ROOT",
+    )
+
+    # Optional: specify which scripts to run
+    parser.add_argument(
+        "-s",
+        "--scripts",
+        action="store",
+        dest="scripts",
+        type=str,
+        nargs="+",
+        default=[
+            "monthly.sh",
+        ],
+        help="Scripts to submit to slurm",
+    )
+
+    # Optional: is this a dry-run? If so, don't submit anything
+    parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        dest="dryrun",
+        help="If true, do not actually submit job",
+    )
+
+    # Optional: By default, slurm will email users when jobs start and finish
+    parser.add_argument(
+        "--no-mail",
+        action="store_false",
+        dest="send_mail",
+        help="If true, send SLURM emails to {user}@ucar.edu",
+    )
+
+    return parser.parse_args()
+
+
+###################
+
+def launch_jobs(start_year, end_year):
+    """
+        Function that calls sbatch with correct options
+    """
+    for component in args.components:
+        for script in args.scripts:
+            job = f"{script.split('.')[0]}_{component}_{job_portion}"
+            logbase = f"logs/{job}"
+            print(f"Submitting {script} for years {start_year} through {end_year} of {case} as {job}...")
+            slurm_opts = f"{mail_opt} --job-name {job} --dependency=singleton"
+            slurm_opts += f" -o {logbase}.out.%J -e {logbase}.err.%J"
+            script_opts = f"{case} {archive_root} {start_year} {end_year} {component}"
+            cmd = f"sbatch {slurm_opts} {script} {script_opts}"
+            if not args.dryrun:
+                # note: the --dependency=singleton option means only one job per job name
+                #       Some jobs had been crashing, and I think it was due to temporary
+                #       files clobbering each other? But only having one year for each
+                #       component / time period seemed to do the trick
+                os.system(cmd)
+            else:
+                print(f"Command to run: {cmd}")
+
+###################
+
+if __name__ == "__main__":
+    args = _parse_args()
+    archive_root = args.archive_root
+    mail_opt = (
+        f"--mail-type=ALL --mail-user={os.environ['USER']}@ucar.edu"
+        if args.send_mail
+        else "--mail-type=NONE"
+    )
+
+    ens_id = f"{args.ensemble_member:03}"
+    if args.start_year < 2006:
+        case = f"b.e11.B20TRC5CNBDRD_no_pinatubo.f09_g16.{ens_id}"
+        job_portion = "20TR"
+        launch_jobs(args.start_year, min(args.end_year, 2005))
+    if args.end_year > 2005:
+        case = f"b.e11.BRCP85C5CNBDRD_no_pinatubo.f09_g16.{ens_id}"
+        job_portion = "RCP"
+        launch_jobs(max(2006, args.start_year), args.end_year)
