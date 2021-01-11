dff99ee300a3c91201af02743c84f7d8d97edf35
Michael Levy <mike.levy.work@gmail.com>
2021-01-11 13:34:14 -0700

    More flexibility in globus script
    Doesn't currently submit the globus transfer request, but does generate file
    list for every component / stream pair and the number of files per transfer
    matches the expectation from the spreadsheet.

--- data_reshaper/transfer_to_globus.sh
+++ data_reshaper/transfer_to_globus.sh
@@ -17,12 +17,73 @@ bad_component_freq() {
 ################
 
 # Determine stream name from component and freq
-get_stream() {
+get_streams() {
   case ${component} in
+    "atm" )
+      case ${freq} in
+        "daily" )
+          streams="cam.h1"
+        ;;
+        "hourly6" )
+          streams="cam.h2"
+        ;;
+        "monthly" )
+          streams="cam.h0"
+        ;;
+        * )
+          bad_component_freq
+        ;;
+      esac
+      ;;
+    "ice" )
+      case ${freq} in
+        "daily" )
+          streams="cice.h1"
+        ;;
+        "monthly" )
+          streams="cice.h"
+        ;;
+        * )
+          bad_component_freq
+        ;;
+      esac
+      ;;
+    "lnd" )
+      case ${freq} in
+        "daily" )
+          streams="clm2.h1"
+        ;;
+        "monthly" )
+          streams="clm2.h0"
+        ;;
+        * )
+          bad_component_freq
+        ;;
+      esac
+      ;;
     "ocn" )
       case ${freq} in
+        "annual" )
+          streams="pop.h.ecosys.nyear1"
+        ;;
+        "daily" )
+          streams="pop.h.nday1 pop.h.ecosys.nday1"
+        ;;
         "monthly" )
-          stream="pop.h"
+          streams="pop.h"
+        ;;
+        * )
+          bad_component_freq
+        ;;
+      esac
+      ;;
+    "rof" )
+      case ${freq} in
+        "daily" )
+          streams="rtm.h1"
+        ;;
+        "monthly" )
+          streams="rtm.h0"
         ;;
         * )
           bad_component_freq
@@ -48,22 +109,30 @@ gen_file_list() {
 
   component=$1
   freq=$2
-  stream=
-  get_stream
+  streams=
+  get_streams
 
-  SRC_ROOT=${GLADE_ROOT}/${MEMBER}/${stream}/proc/COMPLETED
-  DEST_ROOT=${CAMPAIGN_ROOT}/${component}/proc/tseries/${freq}
   rm -f globus_transfer_list.txt
+  DEST_ROOT=${CAMPAIGN_ROOT}/${component}/proc/tseries/${freq}
   for var in `ls ${DEST_ROOT}` ; do
-    if [ -d ${DEST_ROOT}/${var} ]; then
+    if [ ! -d ${DEST_ROOT}/${var} ]; then
+      echo "${DEST_ROOT}/${var} is a file, not directory"
+      continue
+    fi
+    VAR_FOUND=FALSE
+    for stream in ${streams} ; do
+      if [ "${VAR_FOUND}" == "TRUE" ]; then
+        continue
+      fi
+      SRC_ROOT=${GLADE_ROOT}/${MEMBER}/${stream}/proc/COMPLETED
       file=`cd ${SRC_ROOT} ; ls *.${var}.*.nc 2>/dev/null`
       if [ "${file}" ]; then
         echo "${file} $var/$file" >> globus_transfer_list.txt
-      else
-        echo "No file for ${var}"
+        VAR_FOUND=TRUE
       fi
-    else
-      echo "${DEST_ROOT}/${var} is a file, not directory"
+    done
+    if [ "${VAR_FOUND}" != "TRUE" ]; then
+      echo "No file for ${var} in ${streams}"
     fi
   done
 }
@@ -78,12 +147,16 @@ transfer() {
   #   $2: additional opts (optional)
   label=${1//./_} # replace . with _ for label
   echo ${label}
-  echo "Copying from ${SRC_ROOT} to ${DEST_ROOT}"
-  globus transfer $OPTS $2 --label $label ${GLADE}:${SRC_ROOT} ${CAMPAIGN}:${DEST_ROOT} < globus_transfer_list.txt
-  rm globus_transfer_list.txt
+  echo "Copying from ${SRC_ROOT} to ${DEST_ROOT} (${label})"
+#  globus transfer $OPTS $2 --label $label ${GLADE}:${SRC_ROOT} ${CAMPAIGN}:${DEST_ROOT} < globus_transfer_list.txt
+#  rm globus_transfer_list.txt
 }
 
-################
+###############
+#             #
+# MAIN SCRIPT #
+#             #
+###############
 
 # Must have globus-cli installed and must be on casper
 globus --version > /dev/null 2>&1 && GLOBUS_FOUND=TRUE
@@ -105,7 +178,6 @@ CAMPAIGN="6b5ab960-7bbf-11e8-9450-0a6d4e044368"
 OPTS="--sync-level checksum --preserve-mtime --verify-checksum --notify on --batch"
 GLADE_ROOT=/glade/scratch/mlevy/reshaper
 CAMPAIGN_ROOT=/glade/campaign/univ/udeo0005/cesmLE_no_pinatubo
-MEMBER=b.e11.B20TRC5CNBDRD_no_pinatubo.f09_g16.001
 
 # Activate globus
 cmd="globus endpoint activate --web ${GLADE}"
@@ -114,10 +186,80 @@ echo "\$ ${cmd}"
 ${cmd}
 read -p "Log in to globus via the above URL then press enter to continue..."
 
-if [ ! -d ${GLADE_ROOT}/${MEMBER} ]; then
-  echo "ERROR: can not find ${MEMBER} in ${GLADE_ROOT}"
-  exit 1
-fi
+for ensid in {001..035}
+do
+  for compset in B20TRC5CNBDRD BRCP85C5CNBDRD
+  do
+    MEMBER=b.e11.${compset}_no_pinatubo.f09_g16.${ensid}
+    if [ ! -d ${GLADE_ROOT}/${MEMBER} ]; then
+      echo "Can not find ${MEMBER} in ${GLADE_ROOT}"
+      continue
+    fi
 
-gen_file_list ocn monthly
-transfer ${stream}
+    #################################################
+    # Transfer all time series                      #
+    # For each component, glob_ avoids over-writing #
+    # global scope variables in gen_file_list       #
+    #################################################
+
+    # CAM output
+    glob_component=atm
+    for glob_stream in daily hourly6 monthly
+    do
+      echo "Compiling list for ${glob_stream} in ${glob_component}..."
+      gen_file_list ${glob_component} ${glob_stream}
+      wc -l globus_transfer_list.txt
+      transfer "${glob_component}_${glob_stream}"
+    done
+
+    # CICE output
+    glob_component=ice
+    for glob_stream in daily monthly
+    do
+      echo "Compiling list for ${glob_stream} in ${glob_component}..."
+      gen_file_list ${glob_component} ${glob_stream}
+      wc -l globus_transfer_list.txt
+      transfer "${glob_component}_${glob_stream}"
+    done
+
+    # CLM output
+    glob_component=lnd
+    for glob_stream in daily monthly
+    do
+      echo "Compiling list for ${glob_stream} in ${glob_component}..."
+      gen_file_list ${glob_component} ${glob_stream}
+      wc -l globus_transfer_list.txt
+      transfer "${glob_component}_${glob_stream}"
+    done
+
+    # POP output
+    glob_component=ocn
+    for glob_stream in annual daily monthly
+    do
+      echo "Compiling list for ${glob_stream} in ${glob_component}..."
+      gen_file_list ${glob_component} ${glob_stream}
+      wc -l globus_transfer_list.txt
+      transfer "${glob_component}_${glob_stream}"
+    done
+
+    # RTM output
+    glob_component=rof
+    for glob_stream in daily monthly
+    do
+      echo "Compiling list for ${glob_stream} in ${glob_component}..."
+      gen_file_list ${glob_component} ${glob_stream}
+      wc -l globus_transfer_list.txt
+      transfer "${glob_component}_${glob_stream}"
+    done
+
+    ##########################
+    # Transfer restart files #
+    ##########################
+
+
+
+    ######################
+    # Transfer log files #
+    ######################
+  done
+done
